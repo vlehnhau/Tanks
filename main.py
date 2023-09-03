17868270ef310e5b73ca7bf45b8c8670e8e26e39
Mika <malvesbr@students.uni-mainz.de>
2023-09-03 21:26:38 +0200

    standardMap + checkGround Funktion

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import random
 import sys
-
-from PyQt6 import QtGui
-from PyQt6.QtCore import Qt, QTimer, QRect, QCoreApplication, QMetaObject
-from PyQt6.QtGui import QColor, QPixmap
-from PyQt6.QtWidgets import QApplication, QFormLayout, QWidget, QCheckBox, QPushButton, QSlider, QLabel, QMainWindow, \
-    QMenuBar, QStatusBar, QMenu, QMessageBox
+import numpy as np
+from PyQt5 import QtGui
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+import standardMap
 
 class Game():
     pass
@@ -22,35 +22,55 @@ class Window(QWidget, object):
         self.timer = QTimer()
 
         # Outer win settings
-        self.setGeometry(10, 30, 100, 100)
+        self.setGeometry(10, 30, 1000, 400)
         self.setWindowTitle("Game")
 
         self.form = QFormLayout()  # Layout
 
-        # creat board
-        board = QtGui.QPixmap(50, 50)
-        board.fill(QColor(100, 100, 255))
+        # create board
+        self.board = QtGui.QPixmap(1000, 400)
 
-        # Screen
+
         self.display = QLabel()
+        # self.display.setGeometry(QRect(0, 0, 1000, 400))
+        self.display.setAutoFillBackground(True)
+        palette = self.display.palette()
+        palette.setColor(QPalette.Window, QColor(137, 207, 240))  # Set the background color to blue
+        self.display.setPalette(palette)
 
         # Widget und Layout hinzufügen
         self.form.addWidget(self.display)
         self.setLayout(self.form)
 
+        # world
+        self.world = standardMap.worldData()
+        self.world_img = QImage(self.world.data, 1000, 400, QImage.Format_RGBA8888)
+
+        self.mappainter = QPainter(self.world_img)
+        # self.mappainter.setCompositionMode(QPainter.CompositionMode_Clear)
+        self.mappainter.setPen(QColor(137, 207, 240, 255))
+        self.mappainter.setBrush(QColor(137, 207, 240, 255))
+
+        #Hiermit werden später die Krater gezeichnet
+        #Maybe Problem: Panzer werden auch überzeichnet, aber man könnte ja die Panzer dann wieder darüber malen
+        self.mappainter.drawEllipse(QPoint(50, 234), 50, 50)
         # timer
         self.timerFun()
 
+        #Nur zum Testen der checkGround funktion
+        print(self.checkGround(10,10))      #False
+        print(self.checkGround(500, 399))   #True
+        print(self.checkGround(50,234))     #False
+        print(self.checkGround(10,234))     #False
+        print(self.checkGround(50, 285))    #True
+
     def onRepeat(self):
-        width, hight = 600, 400
+        img = self.board.toImage()
 
-        board = QtGui.QPixmap(120, 60)
-        board.fill(QColor(0, 0, 255))
+        self.display.setPixmap(QPixmap.fromImage(self.world_img))
 
-        img = board.toImage()
 
-        scaledBoardImg = img.scaled(width, hight, Qt.AspectRatioMode.KeepAspectRatio)
-        self.display.setPixmap(QPixmap.fromImage(scaledBoardImg))
+
 
     def timerFun(self):
         # timer
@@ -59,6 +79,23 @@ class Window(QWidget, object):
         self.timer.setInterval(150)  # in milliseconds and controls the speed
         self.timer.timeout.connect(self.onRepeat)
         self.timer.start()
+
+
+
+    def checkGround(self, x, y):
+        #Funktion um zu Überprüfen, ob ein Pixel Boden oder Himmel ist.
+        #ABER:
+        #Maybe Problem in der Zukunft: Wenn ein Panzer den Boden Überdeckt, wird der Boden nicht mehr erkannt (da die Farbe des Pixel abgefragt wird)
+        #Aber: juckt mich doch nicht, Viktor muss das mit den Panzern machen hehe
+        #Maybe muss man anderes Bild drüber legen oder so, KP ob QLabel das kann
+        pixel_value = self.world_img.pixel(x,y)
+        color = QColor(pixel_value)
+        if color == QColor(128, 128, 128, 255):
+            return True
+        else:
+            return False
+
+
 
 
 
