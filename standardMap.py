d16b884b9b0baf00dc3224f293663b9b59dd1a92
Mika <malvesbr@students.uni-mainz.de>
2023-09-03 21:26:27 +0200

    standardMap + checkGround Funktion

---
+++ standardMap.py
@@ -0,0 +1,32 @@
+import numpy as np
+from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtWidgets import QLabel, QApplication
+import sys
+
+# Größe der Fläche
+width = 1000
+height = 400
+
+# Erstellen eines leeren 2D Numpy-Arrays mit den Dimensionen width x height und 3 Kanälen (RGB)
+world = np.zeros((height, width, 4), dtype=np.uint8)
+
+# Erstellen einer Sinuskurve
+x_vals = np.arange(width)
+y_vals = np.sin(2* np.pi * x_vals / width) * 0.5  # Sinuskurve
+
+# Schleife über die Pixel und setzen der Farben entsprechend der Sinuskurve
+for x in range(width):
+    for y in range(height):
+        if y <= int((y_vals[x] + 1) * height/2):
+            pass #world[y, x] = [137, 207, 240, 100]  # Blau für Punkte oberhalb der Sinuskurve
+        else:
+            world[y, x] = [128, 128, 128, 255]  # Grau für Punkte unterhalb der Sinuskurve
+
+
+
+# Konvertiere das 2D Numpy-Array in ein QImage
+# world_img = QImage(world.data, width, height, QImage.Format_RGB888)
+
+def worldData():
+    return world
+
