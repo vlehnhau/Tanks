08b5a708c44db2370877ed143a7213de2b0e4616
Mika <malvesbr@students.uni-mainz.de>
2023-10-16 23:13:33 +0200

    Kanonenrohre (lassen sich auch bewegen)

--- main.py
+++ main.py
@@ -12,11 +12,12 @@ class Game():
 
 
 class Player:
-    def __init__(self, color, x, y):
+    def __init__(self, color, x, y, ang):
         self.pLColor = color
         self.pX = x
         self.pY = y
         self.fuel = 100
+        self.angle = ang
 
     # Bewegung des Spielers. Man kann nicht über den Rand hinaus fahren
     def move(self, direction):
@@ -29,6 +30,13 @@ class Player:
                 if self.pX != 21:
                     self.pX -= 1
                     self.fuel -= 1
+
+    # Winkel ändern
+    def changeAngle(self, direction):
+        if direction == "RIGHT":
+            self.angle += 1
+        if direction == "LEFT":
+            self.angle -= 1
 
 
 
@@ -74,12 +82,14 @@ class Window(QWidget, object):
         ### Player Left (grün)
         self.player_left = Player(QColor(0, 150, 0, 255),
                                   110,
-                                  round(int((np.sin(2 * np.pi * 110 / 1000) * 0.5 + 1) * 400/2)))
+                                  round(int((np.sin(2 * np.pi * 110 / 1000) * 0.5 + 1) * 400/2)),
+                                  -45)
 
         ### Player Right (red)
         self.player_right = Player(QColor(180, 0, 0, 255),
                                   900,
-                                  round(int((np.sin(2 * np.pi * 900 / 1000) * 0.5 + 1) * 400 / 2)))
+                                  round(int((np.sin(2 * np.pi * 900 / 1000) * 0.5 + 1) * 400 / 2)),
+                                  -135)
 
 
 
@@ -113,14 +123,57 @@ class Window(QWidget, object):
         temppainter = QPainter(self.temp_img)
 
         # Player Left zeichnen
-        temppainter.setPen(QColor(137, 207, 240, 255))
+        temppainter.setPen(Qt.black)
         temppainter.setBrush(self.player_left.pLColor)
         temppainter.drawRect(self.player_left.pX-20, self.player_left.pY, 40, -25)
 
+        ### Rohr zeichen (Player Left)
+        # Um diesen Punkt rotiert sich das Rohr
+        fixed_x, fixed_y = self.player_left.pX , self.player_left.pY -12.5
+
+        temppainter.setBrush(QColor(0, 90, 0, 255))
+
+        # Rotation
+        transform = QTransform()
+        transform.translate(fixed_x, fixed_y)
+        temppainter.setTransform(transform)
+        transform.rotate(self.player_left.angle)
+        temppainter.setTransform(transform)
+
+        # Rohr
+        temppainter.drawRect(0, -2, 30, 5)
+        # kleiner Zipfel am Rohr
+        temppainter.drawRect(21, -4, 9, 9)
+        # Transformation wieder zurück setzten
+        temppainter.resetTransform()
+
+
         # Player Right zeichnen
-        temppainter.setPen(QColor(137, 207, 240, 255))
         temppainter.setBrush(self.player_right.pLColor)
         temppainter.drawRect(self.player_right.pX - 20, self.player_right.pY, 40, -25)
+
+        ### Rohr zeichen (Player Right)
+        # Um diesen Punkt rotiert sich das Rohr
+        fixed_x, fixed_y = self.player_right.pX, self.player_right.pY - 12.5
+
+        temppainter.setBrush(QColor(120, 0, 0, 255))
+
+        # Rotation
+        transform = QTransform()
+        transform.translate(fixed_x, fixed_y)
+        temppainter.setTransform(transform)
+        transform.rotate(self.player_right.angle)
+        temppainter.setTransform(transform)
+
+        # Rohr
+        temppainter.drawRect(0, -2, 30, 5)
+        # kleiner Zipfel am Rohr
+        temppainter.drawRect(21, -4, 9, 9)
+        # Transformation wieder zurück setzten
+        temppainter.resetTransform()
+
+
+
 
         # temp zeichnen
         self.display.setPixmap(QPixmap.fromImage(self.temp_img))
@@ -154,7 +207,7 @@ class Window(QWidget, object):
         else:
             pass
 
-        # Spiler bewegen falls möglich
+        # Spieler bewegen falls möglich
         if  QKeyEvent.key() == Qt.Key.Key_Right:
             if self.checkIfMovePossible(player.pX+1, player.pY):
                 player.move("RIGHT")
@@ -163,6 +216,13 @@ class Window(QWidget, object):
             if self.checkIfMovePossible(player.pX-1, player.pY):
                 player.move("LEFT")
                 self.fixY(player)
+
+        # angle ändern (Taste UP = Rechts, Taste Down = Links (Wie Blinker))
+        elif QKeyEvent.key() == Qt.Key_Up:
+            player.changeAngle("RIGHT")
+        elif QKeyEvent.key() == Qt.Key_Down:
+            player.changeAngle("LEFT")
+
 
         # Nächste Runde wenn "Space" (später schießen)
         elif QKeyEvent.key() == Qt.Key.Key_Space:
